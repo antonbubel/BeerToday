6a90ae788ff322b4f4170a58e95a4794aa56e539
Anton Bubel <a.bubel@itransition.com>
2019-11-20 00:06:02 +0300

    Configure client-web project with @angular/material and @angular/pwa

--- client/apps/client-web/src/app/app.module.ts
+++ client/apps/client-web/src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    BrowserAnimationsModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
